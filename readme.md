a10cc5a1d03922f70f16391c20a2cebe88aacb9e
Marcos Soares <marcossaore@gmail.com>
2022-06-14 16:20:35 -0300

    initial commit

---
+++ readme.md
@@ -0,0 +1,20 @@
+# Aprendendo conceitos de API
+
+## Instalação Node JS
+
+1 - Vá até o site do node e baixe a última versão estável neste [link](https://nodejs.org/en/).
+
+2 - No diretório raiz da aplicação, execute o comando:
+
+```node
+npm install
+```
+3 - Depois execute o comando:
+
+```node
+npm start
+```
+
+4 - Pronto o servidor estará rodando na porta 5000.
+
+[http://localhost:5000](http://localhost:5000)
---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const users = [];
+const app = express();
+app.use(express.json());
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.status(200).send("Estou funcionado :)");
+});
+
+app.get('/users', (req, res) => {
+    if (users.length === 0) {
+        return res.status(204).send();
+    }
+    res.status(200).json({
+        users
+    })
+});
+
+app.get('/users/:id', (req, res) => {
+    res.status(200).json({
+        user: users[req.params.id - 1]
+    })
+});
+
+app.post('/users', (req, res) => {
+    const { nome, email, idade } = req.body;
+    if (!nome || !email || !idade) {
+        return res.status(400).json({
+            error: 'Dados insuficientes'
+        });
+    }
+    const id = users.length + 1;
+    users.push({ id, nome, email, idade });
+    res.status(200).json({
+        message: 'Usuário criado com sucesso',
+        id
+    })
+});
+
+app.put('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const { nome, email, idade } = req.body;
+    if (!nome || !email || !idade) {
+        return res.status(400).json({
+            error: 'Dados insuficientes'
+        });
+    }
+    users[req.params.id - 1] = { nome, email, idade };
+    res.status(200).json({
+        message: 'Usuário atualizado com sucesso',
+        id
+    })
+});
+
+app.listen(port, () => {
+    console.log('Servidor rodando na porta ' + port);
+})
