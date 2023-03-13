afdc6c8850220eddea3cb1b11fca071c3a7db02c
Marcos Soares <marcossaore@gmail.com>
2023-03-13 12:09:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const users = [];
 const app = express();
 app.use(express.json());
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.status(200).send("Estou funcionado :)");
